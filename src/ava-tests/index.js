fea31af74b6121fb400fef0cbee0275f999b2a9f
Dawson Botsford <dawsonb@uber.com>
2017-05-02 17:57:19 -0700

    ava testing working

--- src/ava-tests/index.js
+++ src/ava-tests/index.js
@@ -7,29 +7,42 @@ import path from 'path';
 const jscodeshiftExecutable = path.resolve('node_modules/.bin/jscodeshift');
 const baseDir = path.join(__dirname, '..');
 const fixturePath = path.join(baseDir, '__testfixtures__');
+const tempDir = tempDirCp(fixturePath);
 
 const fixtures = {
-    'r-dom-to-react-create-element': {},
-    'replace-require': {
-      toReplace: 'object.omit',
-      replaceWith: 'just-omit'
-    },
-    'xtend-to-spread': {}
+  'r-dom-to-react-create-element': {},
+  'replace-require': {
+    toReplace: 'object.omit',
+    replaceWith: 'just-omit'
+  },
+  'xtend-to-spread': {}
 };
 
-test('title', async t => {
-  const tempDir = tempDirCp(fixturePath);
-  // const fixture = path.join(tempDir, 'r-dom-to-react-create-element');
-  // console.log('tempDir: ', tempDir);
-  const fixtureName = 'replace-require';
-  const fixture = path.join(tempDir, fixtureName);
-  const codeModPath = path.join(baseDir, fixture)
-  const args = [`${fixture}.input.js`, '-t', path.join(baseDir, `${fixtureName}.js`)];
-  args.push('--toReplace', toReplace);
-  args.push('--replaceWith', replaceWith);
+// convert flag object to linux-friendly flags
+const flagify = (flags) => {
+  return Object.keys(flags).reduce((acc, key) => {
+    acc.push(`--${key}="${flags[key]}"`);
+    return acc;
+  }, []);
+};
 
-  const res = execa(jscodeshiftExecutable, args);
-  // const res = execa(jscodeshiftExecutable, [`${fixture}.input.js`, '-t', path.join(baseDir, `${fixtureName}.js`)]);
-  const {stdout} = await res;
-  t.regex(stdout, /0 errors/);
+test('flagify helper function', t => {
+  t.deepEqual(flagify({a: 'b'}), ['--a="b"']);
+  t.deepEqual(flagify({a: 'b', c: 'd'}), ['--a="b"', '--c="d"']);
+});
+
+const verifyCodemod = (t, modName) => {
+  const fixture = path.join(tempDir, modName);
+  const args = ['-t', path.join(baseDir, `${modName}.js`),
+    `${fixture}.input.js`, ...flagify(fixtures[modName])];
+  const res = execa.sync(jscodeshiftExecutable, args);
+  t.regex(res.stdout, /0 errors/, `Expected no errors for "${modName}" codemod`);
+};
+
+test('test each', t => {
+  const keys = Object.keys(fixtures);
+  t.plan(keys.length);
+  keys.forEach(modName => {
+    verifyCodemod(t, modName);
+  });
 });
